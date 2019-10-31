79a3b168d928ad1fc9cb09e0d642939170d9495a
Anfas PV <pvanfas.talrop@gmail.com>
2019-10-31 11:06:30 +0530

    update

---
+++ django.py
@@ -0,0 +1,150 @@
+source: https://books.agiliq.com/projects/djenofdjango/en/latest/index.html
+
+#setting up virtual environment (python 2)
+mkdir project && cd project && mkdir src
+virtualenv venv
+source venv/bin/activate
+cd src && pip install django==1.11.15 && pip install pillow psycopg2
+django-admin.py startproject project
+cd project && mkdir static media templates
+python manage.py runserver
+
+#setting up virtual environment (python 3)
+mkdir project && cd project && mkdir src
+virtualenv venv -p python3
+source venv/bin/activate
+cd src && pip install django && pip install pillow
+django-admin.py startproject project
+cd project && mkdir static media templates
+python manage.py runserver
+
+#start a new app
+python manage.py startapp web
+
+#register in INSTALLED APPS
+'web'
+
+#define template directory in TEMPLATES
+'templates'
+
+#define DATABASES
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbname',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST':'localhost',
+        'PORT': '',
+    }
+}
+
+#set media url
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_URL = '/static/'
+STATIC_FILE_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+#define urlpatterns in project/urls.py
+from django.conf.urls import url ,include
+from django.contrib import admin
+from django.views.static import serve
+from django.conf import settings
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^',include('web.urls',namespace="web")),
+
+    url(r'^media/(?P<path>.*)$', serve, { 'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, { 'document_root': settings.STATIC_FILE_ROOT}),
+]
+
+admin.site.site_header = "PROJECT Admininistration"
+admin.site.site_title = "PROJECT Admin Portal"
+admin.site.index_title = "Welcome to PROJECT Researcher Portal"
+
+#create web/urls.py and paste the following
+from django.conf.urls import url
+import views
+
+
+urlpatterns = [
+    url(r'^$', views.index,name="index"),
+    #url(r'^about/', views.about,name="about")
+]
+
+#edit web/views.py
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+def index(request):
+	return HttpResponse("Hello World")
+
+#template rendering
+    #Add assets into /static and html into /templates/web
+    #Add {% load static %} after <head> tag and Change directory path to href="{% static 'location' %}"
+    #example:
+        <script src="{% static 'js/script.js' %}"></script>
+
+#change view into render
+def index(request):
+	return render(request,'web/index.html')
+
+#setup database
+sudo su postgres
+createdb project
+createuser user -P
+psql
+grant all privileges on database femme to techpe;
+\q
+exit
+
+#django models
+from django.utils.translation import ugettext_lazy as _
+from django.db import models
+
+class Blog(models.Model):
+    heading = models.CharField(max_length=128)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/blog')
+    time = models.DateTimeField()
+    video_url = models.URLField()
+
+    class Meta:
+        db_table = 'web_blog'
+        verbose_name = ('Blog')
+        verbose_name_plural = ('Blogs')
+
+    def __unicode__(self):
+        return str(self.pk)
+
+# Import model and define list display in admin.py
+from __future__ import unicode_literals
+from django.contrib import admin
+from web.models import Blog
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('heading','content','image','time','video_url')
+
+admin.site.register(Blog,BlogAdmin)
+
+# migrate changes into app and database
+python manage.py makemigrations
+python manage.py migrate
+
+# Add superuser
+python manage.py createsuperuser
+
+#You have successfully configured the basics......................................................
