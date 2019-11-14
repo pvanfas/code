fdc77e669f5aa143d636bd6cc2d249ea3aaece48
Anfas PV <pvanfas.talrop@gmail.com>
2019-11-14 09:22:25 +0530

    update

---
+++ ubuntu/install.py
@@ -0,0 +1,16 @@
+#Install atom text editor
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get install atom
+
+# Install vscode
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
+
+# Install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
