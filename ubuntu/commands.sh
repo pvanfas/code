8253c1d280ca737ee69ce574a831f60ea82a52da
Anfas PV <pvanfas.talrop@gmail.com>
2019-12-11 11:45:55 +0530

    update

--- ubuntu/commands.sh
+++ ubuntu/commands.sh
@@ -3,3 +3,9 @@ ffmpeg -i python.mov -codec copy python.mkv
 
 # merge video files
 mkvmerge -o output.mkv 'python 1.mkv' \+ python.mkv
+
+# Delete all pyc files
+find . -path "*.pyc"  -delete
+
+# Delete empty folders
+find . -empty -type d -delete
