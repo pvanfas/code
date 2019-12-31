3bed88cda9a1c07c6679c187ffd8b44217f0192f
ANFAS PV <45325099+pvanfas@users.noreply.github.com>
2019-12-31 11:56:18 +0530

    Update commands.sh

--- ubuntu/commands.sh
+++ ubuntu/commands.sh
@@ -4,8 +4,8 @@ ffmpeg -i python.mov -codec copy python.mkv
 # merge video files
 mkvmerge -o output.mkv 'python 1.mkv' \+ python.mkv
 
-# Delete all pyc files
-find . -path "*.pyc"  -delete
-
 # Delete empty folders
 find . -empty -type d -delete
+
+#Remove all *.pyc files and __pycache__ directories recursively
+find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
