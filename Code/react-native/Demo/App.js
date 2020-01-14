a02e902d8a3558872426a58e6103d891f6a1c970
Anfas PV <care.pva@gmail.com>
2020-01-14 15:45:14 +0530

    update

---
+++ Code/react-native/Demo/App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {} from 'react-native';
+import Home from './components/screens/Home';
+
+class App extends React.Component {
+  state = {};
+  render() {
+    return <Home />;
+  }
+}
+
+export default App;
