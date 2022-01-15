cf272d10850a7defa6f64594d893e50aa05d19b8
PV Anfas <anfaspv.info@gmail.com>
2022-01-15 16:09:47 +0530

    in

--- Django CBV/04_admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.apps import apps
-
-app = apps.get_app_config('products')
-
-for model_name, model in app.models.items():
-    admin.site.register(model)
