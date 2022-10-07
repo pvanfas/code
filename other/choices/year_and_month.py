e70e3dbbb6d063d4e5376e238ba4d67e11754348
PV ANFAS <45325099+pvanfas@users.noreply.github.com>
2022-10-07 22:32:17 +0400

    Create year_and_month.py

---
+++ other/choices/year_and_month.py
@@ -0,0 +1,6 @@
+import calendar
+import datetime
+
+
+YEAR_CHOICES = [(y, y) for y in range(1950, datetime.date.today().year + 2)]
+MONTH_CHOICES = [(str(i), calendar.month_name[i]) for i in range(1, 13)]
